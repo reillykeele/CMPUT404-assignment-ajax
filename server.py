d1f3d63d1809c361b17ff23ac9ac448f65ebf3f4
reillykeele <54957139+reillykeele@users.noreply.github.com>
2022-03-06 18:36:26 -0700

    Start work

--- server.py
+++ server.py
@@ -21,9 +21,11 @@
 #     pip install flask
 
 
+import mimetypes
 import flask
-from flask import Flask, request
+from flask import Flask, request, redirect
 import json
+
 app = Flask(__name__)
 app.debug = True
 
@@ -48,10 +50,10 @@ class World:
     def clear(self):
         self.space = dict()
 
-    def get(self, entity):
-        return self.space.get(entity,dict())
+    def get(self, entity) -> dict:
+        return self.space.get(entity, dict())
     
-    def world(self):
+    def world(self) -> dict:
         return self.space
 
 # you can test your webservice from the commandline
@@ -74,27 +76,34 @@ def flask_post_json():
 @app.route("/")
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
-
-@app.route("/entity/<entity>", methods=['POST','PUT'])
-def update(entity):
-    '''update the entities via this interface'''
-    return None
+    return redirect("/static/index.html")
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    return myWorld.world()
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    return myWorld.get(entity)
+
+@app.route("/entity/<entity>", methods=['POST','PUT'])
+def update(entity):
+    '''update the entities via this interface'''
+    data = flask_post_json()
+    myWorld.set(entity, data)
+
+    if request.method == 'PUT':
+        return myWorld.get(entity)
+
+    return ({}, 200)
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    return ('', 200)
 
 if __name__ == "__main__":
     app.run()
